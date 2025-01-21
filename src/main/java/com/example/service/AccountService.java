54663b3455bd2f90288f09d8afbaba435b6276b2
David Quintanilla <davidqm7@outlook.com>
2025-01-21 19:36:33 +0000

    Saving

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,41 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.AuthenticationException;
+
+import org.h2.security.auth.AuthConfigException;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+
+
+@Service
 public class AccountService {
+    
+    private List<Account> accountList = new ArrayList<>();
+    private Message message; 
+
+    public void register (Account account) 
+    {
+        accountList.add(account); 
+    }
+    
+    public void login(String username, String password) throws AuthenticationException
+    {
+        for(Account account: accountList)
+        {
+            if(account.getUsername().equals(username) && account.getPassword().equals(password))
+            {
+                return;
+            }
+            throw new AuthenticationException("Username or Password are invalid!");
+        }
+
+    }
+
+    
+
 }
