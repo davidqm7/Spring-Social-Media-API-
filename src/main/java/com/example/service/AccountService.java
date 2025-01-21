ad6939f521a1157d1cf43b16c726915cd1c22f17
David Quintanilla <davidqm7@outlook.com>
2025-01-21 21:46:15 +0000

    Saving

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -2,38 +2,71 @@ package com.example.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.naming.AuthenticationException;
 
 import org.h2.security.auth.AuthConfigException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.entity.Account;
-import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+
+
 
 
 @Service
 public class AccountService {
     
-    private List<Account> accountList = new ArrayList<>();
-    private Message message; 
+    private AccountRepository accountRepository;  
 
-    public void register (Account account) 
+    @Autowired
+    public AccountService(AccountRepository accountRepository)
     {
-        accountList.add(account); 
+        this.accountRepository = accountRepository; 
     }
-    
-    public void login(String username, String password) throws AuthenticationException
-    {
-        for(Account account: accountList)
+
+    //Registers an account
+    public Account register (Account account) 
+    {   //checks if username is blank or null
+        if(account.getUsername() == null || account.getUsername().isBlank())
         {
-            if(account.getUsername().equals(username) && account.getPassword().equals(password))
-            {
-                return;
-            }
-            throw new AuthenticationException("Username or Password are invalid!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be blank!");
+        }
+        //checks if the password is null or less than four characters
+        if(account.getPassword() == null || account.getPassword().length() < 4)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password must be at least 4 characters in length"); 
+        }
+        //checks if the username already exists
+        if(accountRepository.findByUsername(account.getUsername()).isPresent())
+        {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists!");
         }
 
+
+        return null;
+    }
+    
+
+    //function to log the user in
+    public Account login(String username, String password) throws AuthenticationException
+    {   //check if the username and password is blank or null
+        if(username == null || username.isBlank() || password == null || password.isBlank())
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username and Password cannot be blank!");
+        }
+
+        //verify it matches an existing an existing account
+        Optional<Account> account = accountRepository.findByUsername(username);
+        if (account.isPresent() && account.get().getPassword().equals(password)) {
+            return account.get();
+        }
+
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password!");
     }
 
     
