5b1fc290ebe644438e7092af03af042734b211b0
davidqm7 <128763329+davidqm7@users.noreply.github.com>
2025-01-29 15:12:24 -0500

    Update SocialMediaApp.java

--- src/main/java/com/example/SocialMediaApp.java
+++ src/main/java/com/example/SocialMediaApp.java
@@ -11,10 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import static org.springframework.boot.SpringApplication.run;
 
 /**
- * This is a class that is used to run your application.
- *
- * DO NOT CHANGE ANYTHING IN THIS CLASS
- *
+ * Class that is used to run your application.
  */
 @SpringBootApplication
 public class SocialMediaApp {
